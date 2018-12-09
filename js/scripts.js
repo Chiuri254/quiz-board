bdb754f017787ecc0b2b71ac85a276b0824b89d3
Chiuri254 <joseshiory@gmail.com>
2018-12-09 14:18:39 +0300

    Add jquery event

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,37 @@ $(document).ready(function(){
 
     $('#q1 #submit').click(function(){
         $('.questionForm').hide();
-        $('#q2').fadeIn(300);
+        $('#q2').fadeIn(600);
+        return false;
+
+    });
+    $('#q2 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q3').fadeIn(600);
+        return false;
+
+    });
+    $('#q3 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q4').fadeIn(600);
+        return false;
+
+    });
+    $('#q4 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q5').fadeIn(600);
+        return false;
+
+    });
+    $('#q5 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q6').fadeIn(600);
+        return false;
+
+    });
+    $('#q6 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#results').fadeIn(600);
         return false;
 
     });
