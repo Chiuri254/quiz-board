ca3c4a179d9977b0988b05a2d43a263391e6231f
Chiuri254 <joseshiory@gmail.com>
2018-12-09 14:09:21 +0300

    Add js into our quiz board

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("quiz connected");
+$(document).ready(function(){
+    // hide all questions
+    $('.questionForm').hide();
+    //show first question
+    $('#q1').show();
+
+    $('#q1 #submit').click(function(){
+        $('.questionForm').hide();
+        $('#q2').fadeIn(300);
+        return false;
+
+    });
+});
