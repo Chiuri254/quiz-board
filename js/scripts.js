b026aed48a27385c3c38743b88486888908a9cfa
Chiuri254 <joseshiory@gmail.com>
2018-12-10 15:09:40 +0300

    edit my js code

--- js/scripts.js
+++ js/scripts.js
@@ -20,91 +20,29 @@ $(document).ready(function () {
 
     $('#q1').show(); //show first question
 
-    $('#q1 #submit').click(function () {
-        $('.questionForm').hide(); // hide all question
-        process('q1'); //process question 1
-        $('#q2').fadeIn(400); //show next question
-        return false;
-
-    });
-    $('#q2 #submit').click(function () {
+    $('.questionForm #submit').click(function () {
+        // Get data attribute
+        current = $(this).parents('form:first').data('question');
+        next = $(this).parents('form:first').data('question') + 1;
+        //Hide all questions
         $('.questionForm').hide();
-        process('q2');
-        $('#q3').fadeIn(400);
+        //show next question
+        $('#q' + next + '').fadeIn(400);
+        process('' + current + '');
         return false;
-
-    });
-    $('#q3 #submit').click(function () {
-        $('.questionForm').hide();
-        process('q3');
-        $('#q4').fadeIn(400);
-        return false;
-
-    });
-    $('#q4 #submit').click(function () {
-        $('.questionForm').hide();
-        process('q4');
-        $('#q5').fadeIn(400);
-        return false;
-
-    });
-    $('#q5 #submit').click(function () {
-        $('.questionForm').hide();
-        process('q5');
-        $('#q6').fadeIn(400);
-        return false;
-
-    });
-    $('#q6 #submit').click(function () {
-        $('.questionForm').hide();
-        process('q6');
-        $('#results').fadeIn(400);
-        return false;
-
     });
 });
 //process the answers
-function process(q) {
-    if (q == "q1") {
-        var submitted = $('input[name=q1]:checked').val();
-        if (submitted == sessionStorage.a1) {
-            score++; // score incremented by one if correct
-        }
+function process(n) {
+    //Get input value
+    var submitted = $('input[name=q' + n + ']:checked').val();
+    if (submitted == sessionStorage.getItem('a' + n + '')) {
+        score = score + point;
     }
-    if (q == "q2") {
-        var submitted = $('input[name=q2]:checked').val();
-        if (submitted == sessionStorage.a2) {
-            score++;
-        }
-    }
-    if (q == "q3") {
-        var submitted = $('input[name=q3]:checked').val();
-        if (submitted == sessionStorage.a3) {
-            score++;
-        }
-    }
-    if (q == "q4") {
-        var submitted = $('input[name=q4]:checked').val();
-        if (submitted == sessionStorage.a4) {
-            score++;
-        }
-    }
-    if (q == "q5") {
-        var submitted = $('input[name=q5]:checked').val();
-        if (submitted == sessionStorage.a5) {
-            score++;
-        }
-
-    }
-    if (q == "q6") {
-        var submitted = $('input[name=q6]:checked').val();
-        if (submitted == sessionStorage.a6) {
-            score++;
-        }
-        $('#results').html('<h3>Your final score is: ' + score + ' out of 6</h3><a href="index.html">Take Quiz Again</a>');
+    if (n == total) {
+        $('#results').html('<h3>Your final score is: ' + score + ' out of ' + highest + '</h3><a href="index.html">Take Quiz Again</a>');
     }
     return false;
 }
-
 // Add event listener
 window.addEventListener('load', init, false);
